4ee8d1800f6aff84a9fb309de2270a428e42ad9f
AlleyChang <yifanchang0602@gmail.com>
2022-08-15 17:02:16 +0800

    feat: add URL validation function and bootswatch

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -3,31 +3,38 @@ const router = express.Router()
 const ShortenUrl = require('../../models/shortenUrl') //model
 const generateShortenUrl = require('../../generateShortenUrl') //產生五位英數字
 const baseUrl = 'https://localhost:3000'
+const validateUrl = require('url-validation')
 
 router.get('/', (req, res) => {
   res.render('index')
 })
 
 //找到回傳，沒找到則新增
-router.post('/', (req, res) => {
+router.post('/', (req, res) => { 
   const urlCode = generateShortenUrl()
   const shortenUrl = baseUrl + '/' + urlCode
   const originalUrl = req.body.url
   const option = { upsert: true }
-  return ShortenUrl.findOneAndUpdate({ url: originalUrl }, {
-    $setOnInsert: {
-      url: originalUrl,
-      new_url: shortenUrl,
-      urlCode: urlCode,
-    }
-  }, option)
-    .lean()
-    .then(data => {
-      const code = data.urlCode
-      const shortUrl = data.new_url
-      return res.render('index', { code , shortUrl})
-    })
-    .catch(err => console.log(err))
+  if (validateUrl(originalUrl)){
+    return ShortenUrl.findOneAndUpdate({ url: originalUrl }, {
+      $setOnInsert: {
+        url: originalUrl,
+        new_url: shortenUrl,
+        urlCode: urlCode,
+      }
+    }, option)
+      .lean()
+      .then(data => {
+        const code = data.urlCode
+        const shortUrl = data.new_url
+        return res.render('index', { code, shortUrl })
+      })
+      .catch(err => console.log(err))
+  } else {
+    return res.render('index', { alert: '請輸入有效網址' })
+  }
+  
+  
 })
 
 module.exports = router
--- routes/modules/newUrl.js
+++ routes/modules/newUrl.js
@@ -1,18 +1,16 @@
 const express = require('express')
 const router = express.Router()
 const ShortenUrl = require('../../models/shortenUrl')
-const generateShortenUrl = require('../../generateShortenUrl')
-const baseUrl = 'https://localhost:3000'
-// https://a-cutter.herokuapp.com/
+
 
 router.get('/:code', (req, res) => {
   const code = req.params.code
-  console.log(typeof(code))
   return ShortenUrl.find()
     .lean()
     .then(data => {
       const filteredUrl = data.find(item => item.urlCode === code )
-      return res.redirect(filteredUrl.url)
+      const originalUrl = filteredUrl.url
+      return res.redirect(originalUrl)
       })
     .catch(err => console.log(err))
 })
